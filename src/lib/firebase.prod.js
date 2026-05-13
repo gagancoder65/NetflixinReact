7f6e41a48262979b827166a5b7ae506124aa99d7
waiyneee <dikshkaushik1111@gmail.com>
2026-05-13 09:32:51 +0530

    Prepare for Vercel deploy

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,14 +5,13 @@ import { seedDatabase } from '../seed';
 
 //we need to somehow seed the database
 
-//we need a config here, put your api details here 
 const config = {
-    apiKey: "AIzaSyD6rfFIRNC4j0YoRtZGi6vLlJ0M2baoffE",
-    authDomain: "netflix-react-9480e.firebaseapp.com",
-    projectId: "netflix-react-9480e",
-    storageBucket: "netflix-react-9480e.appspot.com",
-    messagingSenderId: "124358470674",
-    appId: "1:124358470674:web:e265ab9bc66f7c4e9a3ccb"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebase = Firebase.initializeApp(config);
--- src/request.js
+++ src/request.js
@@ -1,4 +1,4 @@
-const API_KEY = "a3862ca74514590e3971df18caf10d15";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
